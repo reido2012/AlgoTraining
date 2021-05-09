53ac91a1f0c390231bdc58bbd9018562e97cb9dd
Omar Reid <oreid52@googlemail.com>
2021-05-09 19:07:50 +0100

    AC: 1st Submit Fast

--- contests/arc118/tax_inc.py
+++ contests/arc118/tax_inc.py
@@ -1,4 +1,4 @@
-from math import floor
+from math import ceil
 
 def get_int_list():
     return list(map(int, input().split()))
@@ -8,22 +8,6 @@ def get_int():
 
 t, N = get_int_list()
 
-ans_num = 0
-ans = None
-prev_tax_price = None
-price = 1
-
-while True:
-    if ans_num == N:
-        print(ans)
-        break
-
-    curr_tax_price = floor(((100 + t)/100)*price)
-
-    if prev_tax_price and curr_tax_price - prev_tax_price > 1:
-        ans_num +=1
-        # Not sure about below
-        ans = curr_tax_price - 1
-
-    prev_tax_price = curr_tax_price
-    price += 1
+pos = N - 1
+ans = pos + ceil((N * 100)/t)
+print(ans)
